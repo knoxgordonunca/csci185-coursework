cdc7bcdd66e0f95d22674fd6a3d92f889fab5ec0
knoxgordonunca <kgordon1@unca.edu>
2022-09-19 16:41:24 -0400

    9/19

---
+++ lectures/lecture09/01-style-property-demo/main.js
@@ -0,0 +1,40 @@
+
+const makeRed = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'red';
+    document.querySelector('#section1').style.backgroundColor = 'red';
+};
+
+const makeBlue = () => {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector('body').style.backgroundColor = 'blue';
+    document.querySelector('#section2').style.backgroundColor = 'blue';
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('body').style.backgroundColor = 'pink';
+    document.querySelector('#section3').style.backgroundColor = 'pink';
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('body').style.backgroundColor = 'orange';
+    document.querySelector('#section4').style.backgroundColor = 'orange';
+};
+
+const resetAll = () => {
+    // your code here...
+    console.log('Reset all colors');
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('#section1').style.backgroundColor = 'white';
+    document.querySelector('#section2').style.backgroundColor = 'white';
+    document.querySelector('#section3').style.backgroundColor = 'white';
+    document.querySelector('#section4').style.backgroundColor = 'white';
+
+};
+
