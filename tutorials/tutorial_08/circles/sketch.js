29a4fa34ab1c3d772d3bc9eb4afb8d6ee267456a
knoxgordonunca <kgordon1@unca.edu>
2022-10-14 16:41:21 -0400

    10/14

--- tutorials/tutorial_08/circles/sketch.js
+++ tutorials/tutorial_08/circles/sketch.js
@@ -4,12 +4,49 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
 
     // fill('red');
-    noFill();
-    circle(100, 200, 50);
-    circle(100, 250, 50);
-    circle(100, 300, 50);
-    circle(100, 350, 50);
-    circle(100, 400, 50);
+    // noFill();
+    // circle(100, 200, 50);
+    // circle(100, 250, 50);
+    // circle(100, 300, 50);
+    // circle(100, 350, 50);
+    // circle(100, 400, 50);
 
-    drawGrid(canvasWidth, canvasHeight);
+    let count = 1;
+    let x=500;
+    let y=500;
+    let d=1000;
+    let c='red';
+
+    while (count < 50) {
+        if (c=='red') {
+            c='yellow'
+        } else if (c=='yellow') {
+            c='orange'
+        } else if (c=='orange') {
+            c='green'
+        } else if (c=='green') {
+            c='blue'
+        } else if (c=='blue') {
+            c='purple'
+        } else if (c=='purple') {
+            c='red'
+        };
+
+        d=d-(20);
+        x=x+(Math.sin(count*(6.14*2)))*30;
+        y=y+(Math.cos(count*(6.14*2)))*30;
+        count+=1;
+        console.log('-----------------');
+        console.log(count);
+        console.log(x);
+        console.log(y);
+        console.log(d);
+        console.log('-----------------');
+        fill(c);
+        strokeWeight(0);
+        circle(x, y, d);
+    };
+
+    //drawGrid(canvasWidth, canvasHeight);
+
 }
---
+++ tutorials/tutorial_09/pixel-art/sketch.js
@@ -0,0 +1,74 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+
+const heart = [
+    [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 2, 2, 3, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 1, 1, 1],
+    [1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1],
+    [1, 1, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+];
+
+const frank = [
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 2, 2, 2, 2, 2, 0, 0],
+    [0, 0, 2, 2, 2, 2, 2, 0, 0],
+    [0, 0, 2, 1, 2, 1, 2, 0, 0],
+    [0, 0, 2, 2, 2, 2, 2, 0, 0],
+    [0, 2, 3, 3, 3, 3, 3, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 2, 2, 0, 2, 2, 0, 0]
+];
+    
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    drawGrid(canvasWidth, canvasHeight);
+    drawPixelArt(heart,100,100,10)
+    drawPixelArt(heart,300,100,20)
+    drawPixelArt(heart,700,100,30)
+    drawPixelArt(frank,100,300,10)
+    drawPixelArt(frank,300,400,20)
+    drawPixelArt(frank,700,550,30)
+}
+
+
+
+function drawPixelArt(grid, x, y, pix) {
+    let i=0;
+    while (i < grid.length) {
+        drawRow(grid[i], x, y, pix);
+        i++;
+        y+=pix;
+    };
+}
+
+function drawRow (row, topX, topY, pixelWidth) {
+
+    let i = 0;
+    while ( i < row.length) {
+        if (row[i] == 0) {
+            noFill();
+        } else if (row[i] == 1) {
+            fill('hotpink');
+        } else if (row[i] == 2) {
+            fill('black');
+        } else if (row[i] == 3) {
+            fill('white');
+        };
+        square(topX, topY, pixelWidth)
+        topX += pixelWidth;
+        i++;
+    }
+}
